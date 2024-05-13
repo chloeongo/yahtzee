9a0c334be6a6e9d0434769696bb5b00bf0eb780a
chloeongo <chloe.ongolesono@gmail.com>
2024-05-13 10:54:52 +0200

    dice imgs added

---
+++ Game/script.js
@@ -0,0 +1,5 @@
+document.getElementById("diceImg1").innerHTML = dices;
+document.getElementById("diceImg2").innerHTML = dices;
+document.getElementById("diceImg3").innerHTML = dices;
+document.getElementById("diceImg4").innerHTML = dices;
+document.getElementById("diceImg5").innerHTML = dices;
