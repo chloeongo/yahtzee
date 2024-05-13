ecb90afbb944d6e2fd0602ef38cfc0d67b70f84b
chloeongo <chloe.ongolesono@gmail.com>
2024-05-13 13:10:25 +0200

    randomized dices added

--- Game/script.js
+++ Game/script.js
@@ -1,5 +1,26 @@
-document.getElementById("diceImg1").innerHTML = dices;
-document.getElementById("diceImg2").innerHTML = dices;
-document.getElementById("diceImg3").innerHTML = dices;
-document.getElementById("diceImg4").innerHTML = dices;
-document.getElementById("diceImg5").innerHTML = dices;
+function throwDices() {
+  const dices = [
+    { number: 1, image: "../images/1.png" },
+    { number: 2, image: "../images/2.png" },
+    { number: 3, image: "../images/3.png" },
+    { number: 4, image: "../images/4.png" },
+    { number: 5, image: "../images/5.png" },
+    { number: 6, image: "../images/6.png" },
+  ];
+
+  const diceRolled = [];
+  for (let i = 0; i < 5; i++) {
+    const randomDices = Math.floor(Math.random() * dices.length);
+    diceRolled.push(dices[randomDices]);
+  }
+
+  for (let i = 0; i < diceRolled.length; i++) {
+    const diceImgElement = document.getElementById("diceImg" + (i + 1));
+    diceImgElement.src = diceRolled[i].image;
+    diceImgElement.alt = "Die " + diceRolled[i].number;
+  }
+}
+
+function throwCount() {
+  document.getElementById("amountOfThrows").innerHTML;
+}
